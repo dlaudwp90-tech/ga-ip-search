763d869c8b314f9e4ead167f930e17d1c91cfb9c
dlaudwp90-tech <dlaudwp90@gmail.com>
2026-04-09 08:34:11 +0900

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
-}
